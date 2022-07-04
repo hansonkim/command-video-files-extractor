6e44fe02ef38c53d3865e804fe4b3c042edf119f
Hanson Kim <hansonkim82@gmail.com>
2022-07-05 06:29:29 +0900

    change home path with /home/hanson environemt variale

--- mv_torrent.sh
+++ mv_torrent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NOW=$(date +"%Y-%m-%d %T")
-echo "$NOW start " >> /home/pi/mv_torrent.txt
+echo "$NOW start " >> $HOME/mv_torrent.txt
 
-sudo find /media/NAS1/torrents -mindepth 2 -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sudo xargs -i{} mv {} /media/NAS1/torrents -v --backup=numbered >> /home/pi/mv_torrent.txt 
-sudo find /media/NAS1/torrents/ -mindepth 1 -type d | sudo xargs -i{} rm {} -rfv >> /home/pi/mv_torrent.txt
+sudo find /media/NAS1/torrents -mindepth 2 -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sudo xargs -i{} mv {} /media/NAS1/torrents -v --backup=numbered >> $HOME/mv_torrent.txt 
+sudo find /media/NAS1/torrents/ -mindepth 1 -type d | sudo xargs -i{} rm {} -rfv >> $HOME/mv_torrent.txt
 
 NOW=$(date +"%Y-%m-%d %T")
-echo "$NOW end" >> /home/pi/mv_torrent.txt
+echo "$NOW end" >> $HOME/mv_torrent.txt
 
