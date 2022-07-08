c3986f843b18392613caab2868cb5e3cb81754b6
hanson kim <hansonkim82@gmail.com>
2022-07-09 02:21:46 +0900

    fix miss typo

--- mv_video.sh
+++ mv_video.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 NOW=$(date +"%Y-%m-%d %T")
-echo "$NOW start " >> $HOME/v_video.txt
+echo "$NOW start " >> $HOME/mv_video.txt
 
-sudo find $1 -mindepth 2 -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | sudo xargs -i{} mv {} $1 -v --backup=numbered >> $HOME/mv_video.txt 
-sudo find $1 -mindepth 1 -type d | sudo xargs -i{} rm {} -rfv >> $HOME/mv_video.txt
+find $1 -mindepth 2 -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | xargs -i{} mv {} $1 -v --backup=numbered >> $HOME/mv_video.txt 
+find $1 -mindepth 1 -type d | xargs -i{} rm {} -rfv >> $HOME/mv_video.txt
 
 NOW=$(date +"%Y-%m-%d %T")
 echo "$NOW end" >> $HOME/mv_video.txt
